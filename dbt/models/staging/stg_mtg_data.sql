7bad22204d17ae561a86feeb35ee0626e3676743
pwsampanis@gmail.com <pwsampanis@gmail.com>
2023-04-27 04:50:39 +0000

    Fixed staging file, added new columns to raw (source) file and changed final model.

--- dbt/models/staging/stg_mtg_data.sql
+++ dbt/models/staging/stg_mtg_data.sql
@@ -1,13 +1,13 @@
 with mtg_raw as (select * from {{ source("mtg_testing", "mtg_data_raw") }})
 
 select
---    `id`,
-    name,
+    `id` as card_id,
+    name as card_name,
     mana_cost,
-    cmc,
-    type_line,
-    oracle_text,
-    power,
+    cmc as converted_mana_cost,
+    type_line as card_type,
+    oracle_text as card_text,
+    `power`,
     toughness,
     loyalty,
     `set`,
@@ -15,8 +15,10 @@ select
     rarity,
     artist,
     collector_number,
-    color_identity, 
-    IF(REGEXP_CONTAINS(type_line, ".*rtifact*.")=True, type_line,"") as artifact_if_statement
-
-    from mtg_raw
-
+    color_identity,
+    prices.usd as price,
+    prices.usd_foil as foil_price,
+    image_uris.normal as image_url,
+    coalesce(PARSE_DATE('%F',released_at),'2049-01-01') as release_date,
+    if(regexp_contains(type_line, ".*rtifact*.") = true, type_line, "") as is_artifact
+from mtg_raw
